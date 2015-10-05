96b8ff6c7fc03bc5ec67849214fbc5526af76e0f
ma7k <admin@VM-Mac.local>
2015-10-04 23:19:57 -0700

    Finished ui ux touches

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,20 @@
 class PagesController < ApplicationController
-  def home
-  end
+ def home
+	if current_user
+		redirect_to tasks_path
+	end
+end
+
+
+
 
   def about
   end
+
+
+
+
+  def test
+end
+
 end
